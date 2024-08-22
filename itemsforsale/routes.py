febc6b4a0edd5214d6a690c045a916a449bd1b84
Ash Marshall <ashley_marshall86@hotmail.co.uk>
2024-08-22 10:58:53 +0000

    add an Add Categry button and wire up to a category function in order for users to add categories

--- itemsforsale/routes.py
+++ itemsforsale/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from itemsforsale import app, db
 from itemsforsale.models import Category, Item
 
@@ -6,3 +6,18 @@ from itemsforsale.models import Category, Item
 @app.route("/")
 def home():
     return render_template("items.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("add_category.html")
